152a95ce9bf2950a2069257f503c5e0356d85efc
Bruno B. Silva <contato.brunobsilva@outlook.com>
2025-04-02 12:27:27 -0300

    novas alterações

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,28 +4,29 @@ import java.util.Scanner;
 public class App {
         //Comentário
 
-    public static void funcao_teste (){
+    public static void main (String[] arg) throws Exception{
         //Biblioteca para entrada de dados
-        Scanner ler = new Scanner(System.in); 
-
-        //variável local a
-        String v2;
-        int v1;
-
-        System.out.println("Comando de Saida de Dados");
+        Scanner scanner = new Scanner(System.in); 
         
-        System.out.printf("Informe uma palavra: ");
-        v2 = ler.next();
 
-        System.out.printf("Informe um número: ");
-        v1 = ler.nextInt();
+        System.out.println("Digite o Peso: ");
+        float massa = scanner.nextFloat();
+
+        System.out.println("Digite a Altura: ");
+        float altura = scanner.nextFloat();
+
+        scanner.close();
+
+        CorpoHumano corpo = new CorpoHumano(0,0);
+        corpo.setMassa(massa);
+        corpo.setAltura(altura);
+
+        corpo.calcularImc();
+        System.out.println("o imc é de: " + corpo.calcularImc());
+
+        
 
 
     }
-    public static void main(String[] args) throws Exception {
-        CorpoHumano c1=new CorpoHumano(0, 0);
-        c1.setMassa(2);
-
-
-    }
+    
 }
--- src/main/java/CorpoHumano.java
+++ src/main/java/CorpoHumano.java
@@ -6,43 +6,31 @@ public class CorpoHumano {
     private float volume;
     private float altura;
 
-    public CorpoHumano (float massa, float volume)
+    public CorpoHumano (float massa, float altura)
     {
         this.massa = massa;
-        this.volume = volume;
-        this.densidade = (float) massa / volume;
         this.altura = altura;
     }
+
     public float getMassa()
     {
         return massa;
     }
-    public float getVolume()
-    {
-        return volume;
-    }
-    public float getDensidade()
-        {return densidade;}
-
     public float getAltura()
-        {return altura;}
-    
-    public void setMassa(float massa){
-        this.massa = massa;
-        densidade = massa / volume;
+    {
+        return altura;
     }
-    public void setVolume(float volume){
-        this.volume = volume;
-        densidade = massa / volume;
-    }
-    
     public void setAltura(float altura){
         this.altura = altura;
     }
-    public void setDensidade(float densidade){
-        this.densidade = densidade;
-        massa = densidade * volume;
+    public void setMassa(float massa){
+        this.massa = massa;
     }
+    public double calcularImc()
+    {
+      return massa / (altura*altura);
+    }
+
     
     
 }   
